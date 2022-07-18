94fa62198f76a406a0d2e96a7872a8ba00460c20
leo7medina <leo_medina14@hotmail.com>
2022-07-17 23:50:59 -0500

    Test in component father

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Person } from 'src/app/models/person.model';
+import { PersonComponent } from '../person/person.component';
 
 import { PeopleComponent } from './people.component';
 
-describe('PeopleComponent', () => {
+fdescribe('PeopleComponent', () => {
   let component: PeopleComponent;
   let fixture: ComponentFixture<PeopleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PeopleComponent ]
+      declarations: [ PeopleComponent, PersonComponent ]
     })
     .compileComponents();
 
@@ -20,4 +23,37 @@ describe('PeopleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a list app-person components', () => {
+    // Arrange
+    component.people = [
+      new Person('Leonardo', 'Arias', 23, 1, 1),
+      new Person('Valentina', 'Rodriguez', 12, 2, 3),
+      new Person('Santiago', 'Dolores', 12, 2, 3),
+    ];
+    // Act
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person'));
+    // Assert
+    expect(debugElement.length).toEqual(3);
+  });
+
+  it('should raise selected event when clicked', () => {
+    // Arrange
+    component.people = [
+      new Person('Leonardo', 'Arias', 23, 1, 1),
+      new Person('Valentina', 'Rodriguez', 12, 2, 3),
+      new Person('Santiago', 'Dolores', 12, 2, 3),
+    ];
+    const idx = 1;
+    //Act
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person'));
+    const btnDe = debugElement[idx].query(By.css('.btn-choose'));
+    btnDe.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    //
+    expect(component.selectedPerson).toEqual(component.people[idx]);
+  });
+  
 });
--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -9,9 +9,18 @@ import { Person } from 'src/app/models/person.model';
 export class PeopleComponent implements OnInit {
 
   person: Person = new Person('Leonardo', 'Arias', 1, 1, 23);
+  people: Person[] = [
+    new Person('Leonardo', 'Arias', 23, 1, 1),
+    new Person('Valentina', 'Rodriguez', 12, 2, 3),
+  ];
+  selectedPerson: Person | null = null;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChoose(person: Person) {
+    this.selectedPerson = person;
   }
 
 }
